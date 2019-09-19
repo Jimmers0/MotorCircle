b144351dec3a0325403440ed95dbddd867e1a284
Jamie Randall <bentleyx007@gmail.com>
2019-09-19 11:03:50 -0700

    Removed unnecessary files/Begining

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const init = require('./chat').default
+
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
@@ -11,7 +10,7 @@ app.use(express.static('public'))
 app.use('/api', require('./routes/api'))
 app.use('/', require('./routes'))
 
-init(io)
+
 
 server.listen(8000, () => {
   console.log("Server listening on port 8000")
